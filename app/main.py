a0dbbeb1beea3c66c902654d9b78b5cf1353b0d8
Student <bubenkir2007@gmail.com>
2025-09-04 16:00:14 +0500

    После изменения типа данных с int на str в функциях parse_number и rectangle_area, выдаёт ошибку на выполнении функции test_area_3_and_5_negative, она вызывает функцию rectangle_area, которая выдаёт ошибку - ValueError - Сторона должна быть положительным числом

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from tests.test_rectangle_area import test_area_3_and_5_positive, test_area_3_and_5_negatie, test_area_with_empty_string
+
+positive_test = test_area_3_and_5_positive()
+negative_test = test_area_3_and_5_positive()
+empty_string_test = test_area_3_and_5_positive()
+
+print(positive_test)
+print(negative_test)
+print(empty_string_test)
