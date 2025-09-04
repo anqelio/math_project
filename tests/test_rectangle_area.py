36bfc4bf84561f27dd9dd6afde942d00d1cd1ae6
Student <bubenkir2007@gmail.com>
2025-09-04 15:45:04 +0500

    Функция parse_number и rectangle_area принимают в себя str тип данных, в результате чего вылетает ValueError

---
+++ tests/test_rectangle_area.py
@@ -0,0 +1,41 @@
+import pytest
+from app.geometry import rectangle_area
+
+def test_area_3_and_5_positive():
+    """
+    Тест вычисления площади с целыми положительными длинами сторон
+    """
+    # Arrange
+    width = 3
+    height = 5
+    expected_result = 15
+    # Act
+    actual_result = rectangle_area(width, height)
+    # Assert
+    assert actual_result == pytest.approx(expected_result)
+
+def test_area_3_and_5_negatie():
+    """
+    Тест вычисления площади с целыми отрицательными длинами сторон
+    """
+    # Arrange
+    width = -3
+    height = 5
+    expected_result = 15
+    # Act
+    actual_result = rectangle_area(width, height)
+    # Assert
+    assert actual_result == pytest.approx(expected_result)
+
+def test_area_with_empty_string():
+    """
+    Тест вычисления площади с пустой строкой в качестве длины стороны
+    """
+    # Arrange
+    width = ""
+    height = "5"
+    # Act
+    with pytest.raises(ValueError) as exc_info:
+        rectangle_area(width, height)
+    # Assert
+    assert str(exc_info.value) == "Введено недопустимое значение"
